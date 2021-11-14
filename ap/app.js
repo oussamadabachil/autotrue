54f2a406a81f15a19cee850284f88991a899d436
devWorkDin <78686921+devWorkDin@users.noreply.github.com>
2021-11-14 21:07:11 +0100

    first commit

---
+++ ap/app.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector(".menu")
+let burger = document.querySelector(".demo");
+burger.addEventListener("click",()=>{
+
+    burger.classList.toggle("open")
+    menu.classList.toggle("menu_shown")
+    
+    
+})
+
+
